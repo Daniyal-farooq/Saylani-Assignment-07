c3c82844c1e2ab3a0449f9982165e8ca8004890d
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-23 14:46:50 +0500

    string 3

---
+++ string problems/3/app.js
@@ -0,0 +1,3 @@
+var str1 = "This aint";
+var str2 = "a joke";
+console.log(str1.concat(" ", str2));
---
+++ string problems/3/app.ts
@@ -0,0 +1,4 @@
+let str1:string = "This aint"
+
+let str2:string = "a joke"
+console.log(str1.concat(" ",str2))
