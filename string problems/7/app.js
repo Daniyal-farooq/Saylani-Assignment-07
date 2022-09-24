b56eff033c293b8bb42af2ae569e43299e57697a
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-24 15:41:49 +0500

    problem 7 resolved

---
+++ string problems/7/app.js
@@ -0,0 +1,18 @@
+var str = "aBabCD";
+// console.log(str.length);
+var str2 = ["tg", "fh"];
+console.log(str2[1]);
+for (var index = 0; index < str.length; index++) {
+    // console.log(str[index]);
+    var chr = str.charCodeAt(index);
+    if (chr >= 65 && chr <= 90) {
+        chr += 32;
+    }
+    else if (chr >= 97 && chr <= 122) {
+        chr -= 32;
+    }
+    console.log(String.fromCharCode(chr));
+    str2[index] = String.fromCharCode(chr);
+}
+console.log(str2);
+console.log(str2.join(""));
---
+++ string problems/7/app.ts
@@ -0,0 +1,30 @@
+let str:string = "aBabCD"
+// console.log(str.length);
+let str2:string[] = ["tg","fh"]
+console.log(str2[1]);
+
+
+for (let index = 0; index < str.length; index++) {
+        
+    
+        // console.log(str[index]);
+        let chr:number = str.charCodeAt(index)
+        if(chr >= 65 && chr<= 90)
+        {
+            chr+=32
+        }
+        else if ( chr >= 97 && chr<= 122 ){
+            chr-=32
+        }
+        console.log(String.fromCharCode(chr));
+        str2[index] = String.fromCharCode(chr)
+}
+console.log(str2);
+console.log(str2.join(""));
+
+
+
+
+
+
+
