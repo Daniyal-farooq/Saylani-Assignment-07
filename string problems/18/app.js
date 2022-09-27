52f558d118787e026e0ef7c04ae268e44257e1a5
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-27 17:24:12 +0500

    string problem 18

---
+++ string problems/18/app.js
@@ -0,0 +1,27 @@
+var str = prompt("Enter a string");
+console.log(str.length);
+var arr = [];
+for (var index = 0; index < str.length; index++) {
+    var element = str[index];
+    arr[index] = element;
+}
+//finding frequency
+var count = [];
+for (var index = 0; index < arr.length; index++) {
+    count[index] = 0;
+    for (var index2 = 0; index2 < arr.length; index2++) {
+        if (arr[index] == arr[index2]) {
+            count[index] += 1;
+        }
+    }
+}
+var max = count[0];
+var strIndex = 0;
+for (var index = 0; index < str.length; index++) {
+    var element = count[index];
+    if (max < count[index]) {
+        max = count[index];
+        strIndex = index;
+    }
+}
+console.log("The character \"".concat(str[strIndex], "\" has the greatest frequency"));
