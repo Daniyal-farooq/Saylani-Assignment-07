898daffe2ff40bd0c7b2b7597d0e82d9a870f0f4
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-29 12:49:54 +0500

    String Problem 19

---
+++ string problems/19/app.ts
@@ -0,0 +1,33 @@
+let str:string = prompt("Enter a string")
+console.log(str.length);
+
+let arr:string[] = []
+for (let index = 0; index < str.length; index++) {
+    const element = str[index];
+    arr[index] = element
+}
+//finding frequency
+let count:number[] = []
+for (let index = 0; index < arr.length; index++) {
+    count[index] = 0
+    for (let index2 = 0; index2 < arr.length; index2++) {
+        if (arr[index] == arr[index2]){
+            count[index]+=1
+           
+        }
+        
+    }
+    
+}
+let min:number = count[0]
+let strIndex:number = 0 
+for (let index = 0; index < str.length; index++) {
+    const element = count[index];
+    if (min > count[index]){
+        min = count[index]
+        strIndex = index
+    }
+}
+
+
+console.log(`The character \"${str[strIndex]}\" has the lowest frequency`);
