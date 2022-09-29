324f74faf03bfd9e647765582870e26f0d9ce672
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-29 16:02:03 +0500

    Conditional Problem 03

---
+++ conditional operators/3/app.js
@@ -0,0 +1,2 @@
+var n1 = 7;
+n1 % 2 == 0 ? console.log("".concat(n1, " is an even number.")) : console.log("".concat(n1, " is an odd number."));
---
+++ conditional operators/3/app.ts
@@ -0,0 +1,2 @@
+let n1:number = 7
+n1%2==0 ? console.log(`${n1} is an even number.`) :console.log(`${n1} is an odd number.`) ;
