2c8d3fd4c2273384dfac946c92a655c81bbd6a36
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-30 11:58:04 +0500

    String problem 04

---
+++ conditional operators/4/app.js
@@ -0,0 +1,2 @@
+var year = 2002;
+year % 4 == 0 ? console.log("".concat(year, " is a leap year.")) : console.log("".concat(year, " is not a leap year."));
---
+++ conditional operators/4/app.ts
@@ -0,0 +1,2 @@
+let year:number = 2002
+year%4==0 ? console.log(`${year} is a leap year.`) :console.log(`${year} is not a leap year.`) ;
