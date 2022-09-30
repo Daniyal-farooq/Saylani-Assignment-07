1f11d9b811c4a8e66b2264da6b3959f45e3ebc7b
Daniyal-farooq <mdanifarooq@gmail.com>
2022-09-30 12:51:57 +0500

    Switch Problem 02

---
+++ switch problems/2/app.ts
@@ -0,0 +1,43 @@
+let n:number = 8
+switch(n){
+    case (1):
+        console.log("31 days in January.");
+        break;
+    case 2:
+        console.log("28 or 29 days in February.");
+        break;
+    case 3:
+        console.log("31 days in March .");
+        break;
+    case 4:
+        console.log("30 days in April.");
+        break;
+    case 5:
+        console.log("31 days in May.");
+        break;                
+    case 6:
+        console.log("30 days inJune.");
+        break;
+    case 7:
+        console.log("31 days in July.");
+        break;
+    case 8:
+        console.log("31 days in August.");
+        break;
+    case 9:
+        console.log("30 days inSeptember.");
+        break;
+    case 10:
+        console.log("31 days in October.");
+        break;
+    case 11:
+        console.log("30 days inNovember.");
+        break;
+    case 12:
+        console.log("31 days in December.");
+        break;    
+    default:
+        console.log("Invalid number of month.");
+        break;
+
+}
