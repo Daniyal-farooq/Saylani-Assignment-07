250d3c595a52f07281cb38eb9702eb274120f5a0
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-01 14:29:19 +0500

    Switch Problem 06

---
+++ switch problems/6/app.js
@@ -0,0 +1,12 @@
+var n1 = -09;
+switch (true) {
+    case (n1 > 0):
+        console.log("".concat(n1, " is greater than 0."));
+        break;
+    case (n1 < 0):
+        console.log("".concat(n1, " is smaller than 0."));
+        break;
+    default:
+        console.log("It is 0.");
+        break;
+}
---
+++ switch problems/6/app.ts
@@ -0,0 +1,13 @@
+let n1:number = -09 
+switch (true) {
+    case (n1 > 0):
+        console.log(`${n1} is greater than 0.`);
+        break;
+    case (n1 < 0):
+        console.log(`${n1} is smaller than 0.`);
+        break;
+    default:
+        console.log("It is 0.");
+        
+        break;
+}
