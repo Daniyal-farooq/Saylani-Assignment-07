b59903e67d440af7a2198739817fa6a800f7214d
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-02 14:34:49 +0500

    String Problem 25

---
+++ string problems/25/app.js
@@ -0,0 +1,4 @@
+var str = "This is a sample string.";
+var del = "s";
+var rep = "$";
+console.log(str.replace("s", "$"));
---
+++ string problems/25/app.ts
@@ -0,0 +1,4 @@
+let str:string = "This is a sample string."
+let del:string = "s"
+let rep:string = "$"
+console.log(str.replace("s","$"));
