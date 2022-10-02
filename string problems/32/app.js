10e36c23fa7ea73ab75198fd5f03ee90c748b49c
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-02 16:42:39 +0500

    String Problem 32

---
+++ string problems/32/app.js
@@ -0,0 +1,4 @@
+var str = "This is yet another sample string example cz i like string.";
+var wrd = "string";
+str = str.replace(wrd, "");
+console.log(str);
---
+++ string problems/32/app.ts
@@ -0,0 +1,5 @@
+let str:string = "This is yet another sample string example cz i like string."
+let wrd:string = "string"
+str = str.replace(wrd,"")
+console.log(str);
+
