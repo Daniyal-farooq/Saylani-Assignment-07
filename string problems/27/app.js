33ea70a6316bf68ee0f55dd2563e70aa763234c6
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-02 15:29:42 +0500

    String Problem 27

---
+++ string problems/27/app.js
@@ -0,0 +1,23 @@
+var str = "This is not a sample string fellA"; //prompt("Enter a string")
+var ch = "a"; //prompt("Enter a character to find in it")
+var re = "A";
+var srr = [];
+var ind;
+for (var index = 0; index < str.length; index++) {
+    var element = str[index];
+    srr[index] = str[index];
+    if (element == ch) {
+        ind = index;
+        srr[index] = re;
+    }
+}
+// for (let index = 0; index < srr.length; index++) {
+//     const element = srr[index];
+//     if (index == ind) {
+//         srr[index] = re
+//     }
+// }
+console.log(srr.join(""));
+// let str:string = "THis is not a sample."
+// let a:string = str.replace( /([aeiou])/g,"$1$1" )
+// console.log(a);
