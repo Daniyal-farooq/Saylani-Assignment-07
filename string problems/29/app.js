60e06be067a1ac89856f013c7257f8783f1f8ffb
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-02 15:45:52 +0500

    String Problem 28

---
+++ string problems/29/app.js
@@ -0,0 +1,9 @@
+var str = "This is another sample string and string";
+var w = "string";
+var a = str.lastIndexOf(w);
+console.log("The word \"".concat(w, "\" last occurence is at index ").concat(a));
+// console.log(a);
+// let a:string[] = str.split("string")
+// console.log(a);
+// let a =  str.search("stringe")
+// console.log(a);
--- string problems/29/app.ts
+++ string problems/29/app.ts
@@ -1,7 +1,7 @@
 let str:string = "This is another sample string and string"
 let w:string = "string"
-let a:number = str.indexOf(w)
-console.log(`The word "${w}" first occurence is at index ${a}`);
+let a:number = str.lastIndexOf(w)
+console.log(`The word "${w}" last occurence is at index ${a}`);
 
 
 // console.log(a);
